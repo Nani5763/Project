28251c83431db193ab91b83cb484c99f7542464b
pavan <pavansaidevops457@gamil.com>
2025-03-07 16:23:58 +0530

    updates

--- frontend/src/pages/Books.jsx
+++ frontend/src/pages/Books.jsx
@@ -33,7 +33,7 @@ const Books = () => {
 
   return (
     <div>
-      <h1>AWS DevOps Future BOOKS </h1>
+      <h1>Pavan sai Bonam Book Store</h1>
       <div className="books">
         {books.map((book) => (
           <div key={book.id} className="book">
