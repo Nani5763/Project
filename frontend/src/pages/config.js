e19a32e110061fbe50f4d366b6670f2352caed43
pavan <pavansaidevops457@gamil.com>
2025-03-17 14:19:49 +0530

    updates

--- frontend/src/pages/config.js
+++ frontend/src/pages/config.js
@@ -3,6 +3,6 @@
 // export default API_BASE_URL;
 // const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || "http://backend";
 // export default API_BASE_URL;
-const API_BASE_URL = "https://api.bonam.co.in";
+const API_BASE_URL = "http://api.bonam.co.in";
 export default API_BASE_URL;
 
